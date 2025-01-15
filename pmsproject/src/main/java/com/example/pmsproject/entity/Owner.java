bdf18ea73c706156fcb770eb8db0b6c4a1e68b94
aksharavind <akshayaaravind23@gmail.com>
2025-01-15 21:20:23 +0530

    commited

--- pmsproject/src/main/java/com/example/pmsproject/entity/Owner.java
+++ pmsproject/src/main/java/com/example/pmsproject/entity/Owner.java
@@ -1,4 +1,67 @@
 package com.example.pmsproject.entity;
 
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "owner")
 public class Owner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long ownerId;
+    private String username;
+    private String name;
+    private String email;
+    private String address;
+    private String password;
+
+    // Getters and Setters
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
