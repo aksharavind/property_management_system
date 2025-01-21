b072b5077d44f406380d85c9b912bb067bb71990
aksharavind <akshayaaravind23@gmail.com>
2025-01-21 15:11:00 +0530

    commited

--- pmsproject/src/main/java/com/example/pmsproject/Controller/PaymentsController.java
+++ pmsproject/src/main/java/com/example/pmsproject/Controller/PaymentsController.java
@@ -1,4 +1,27 @@
 package com.example.pmsproject.Controller;
 
+
+import com.example.pmsproject.Service.PaymentsService;
+import com.example.pmsproject.entity.Owner;
+import com.example.pmsproject.entity.Payments;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/payments")
 public class PaymentsController {
+@Autowired
+    private PaymentsService paymentsService;
+
+@PostMapping
+    public Payments generatePayments(@RequestBody Payments payments)
+{
+    return paymentsService .generatePayments(payments);
+}
+
+
+
+
 }
--- pmsproject/src/main/java/com/example/pmsproject/Service/PaymentsService.java
+++ pmsproject/src/main/java/com/example/pmsproject/Service/PaymentsService.java
@@ -1,4 +1,19 @@
 package com.example.pmsproject.Service;
 
+
+import com.example.pmsproject.entity.Payments;
+import com.example.pmsproject.repository.PaymentsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PaymentsService {
+    @Autowired
+
+    private PaymentsRepository paymentsRepository;
+    public Payments generatePayments(Payments payments)
+    {
+      return  paymentsRepository.save(payments);
+    }
+
 }
